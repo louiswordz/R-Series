f6bdb4eef46bc0cd6f4f89bb0751511e00c6b316
unknown <louismerit3@gmail.com>
2024-10-21 19:23:47 +0100

    Initial commit

---
+++ file1.R
@@ -0,0 +1,83 @@
+class(" ")
+class(' ')
+
+class(20)
+
+class(20L)
+
+class(11.5)
+
+class(TRUE)
+class(FALSE)
+
+class(12+11i)
+
+# Five data types 
+#Character, Numeric, Integer, Logical and Complex
+
+print("Good day")
+
+# Variables
+name = "Benjamin"
+print(name)
+
+User <- 'Crowny'
+print(User)
+
+"Today is fun with R" -> item
+
+"     Hope you are having fun too"  -> v1
+print(v1)
+
+trimws(v1)
+
+paste(name, User)
+nchar(User)
+
+tolower(User)
+toupper((name))
+
+#Numbers (Numeric and Integers)
+num1 = 10
+num2 = 12L
+print(num1 + num2)
+class(num1+num2)
+
+num2 - num1
+
+num2 * 2
+
+num1/5
+
+11 %/%2
+11.2 ^ 3
+3 ^ 3
+11/2
+
+11 %%2
+
+
+# Logical
+num1 > num2
+num2 == num1
+num2 != num1
+num2 < num1
+num2 <= 2
+num2 >= 5
+
+# Complex
+c <- 11+11i
+c2 <- 12+11i
+
+# Raw
+a <- 'Henry'
+charToRaw(a)
+
+# charMatch
+charmatch('o','ood')
+
+charmatch('H', a)
+
+charmatch('e', a)
+
+
